ecd626276d096b035a79599329d1506730d591c3
Glen Stummer <gstummer@spscommerce.com>
2014-06-20 12:24:31 -0500

    added ubuntu apt sources

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,4 +1,26 @@
+case node[:platform]
 
+when "ubuntu","debian"
+
+execute "logstash_apt_key" do
+  command " wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add - && touch /home/ubuntu/.elasticsearch_key"
+  creates "/home/ubuntu/.elasticsearch_key"
+  action :run
+end
+
+cookbook_file "/etc/apt/sources.list.d/elasticsearch.list" do
+  source "elasticsearch.list"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+execute "update_apt" do
+  command "apt-get update"
+  action :run
+end
+
+when "centos","rhel","oracle"
 #Create the Logstash Official Yum Repo
 yum_repository 'logstash-1.4' do
   description "logstash repository for 1.4.x packages"
@@ -15,4 +37,5 @@ package 'logstash' do
   end
   action :install
   only_if { node['logstash']['install_package'] }
+end
 end
